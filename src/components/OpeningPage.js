7da6d08b25552096cf975bda557f70dd4835ab24
Omurzak Keldibekov <komur.19991008@gmail.com>
2021-11-27 01:47:55 +0600

    add on opening page npm description

--- src/components/OpeningPage.js
+++ src/components/OpeningPage.js
@@ -9,7 +9,7 @@ const NavigationBar = (props) => {
     <div className="opening-screen">
       <div className="developer-description">
         <h1>Developers and Guests!</h1>
-        <p>Create feedback, survey, questionnaire and see the response and statistics of your survey. And if you are a developer, receive POST API request of your survey.</p>
+        <p>Create feedback, survey, questionnaire and see the response and statistics of your survey. And if you are a developer, receive POST API request of your survey. Also, you can get React npm package to place the form on your web page.</p>
         <button onClick={() => props.history.push({pathname: (isAuthenticated) ? "/NewForm" : "/SignIn"})}>Get Started</button>
       </div>
     </div>
